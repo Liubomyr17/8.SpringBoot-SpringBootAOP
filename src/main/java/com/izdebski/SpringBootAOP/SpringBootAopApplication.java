721a6d06e46f57fb2bfcf27a5e71961247a2ac5d
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-12-06 12:54:19 +0200

    Add files via upload

---
+++ src/main/java/com/izdebski/SpringBootAOP/SpringBootAopApplication.java
@@ -0,0 +1,14 @@
+package com.izdebski.SpringBootAOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopApplication.class, args);
+	}
+
+}
+
